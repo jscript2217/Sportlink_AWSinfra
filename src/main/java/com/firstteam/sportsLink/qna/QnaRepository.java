a352364e98af479e610fa79fb43b4d122027edc8
eeeeeni <140162964+eeeeeni@users.noreply.github.com>
2024-08-12 11:48:29 +0900

    Update QnaRepository.java

--- src/main/java/com/firstteam/sportsLink/qna/QnaRepository.java
+++ src/main/java/com/firstteam/sportsLink/qna/QnaRepository.java
@@ -1,9 +1,9 @@
-package com.firstteam.sportsLink.qna;
+package com.firstteam.sportsLink.Qna;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QnaRepository extends JpaRepository<QnaDTO, Long> {
-    // 기타 메서드 정의
+public interface QnaRepository extends JpaRepository<QnaEntity, Long> {
+    // 기타 메서드 정의 (필요 시)
 }
